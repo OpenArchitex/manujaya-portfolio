48329caf72914748224d4da9b9b522a4d0a277c5
Sudharaka <sudharakap@mun.ca>
2022-12-28 10:00:08 -0800

    feat: add node versions

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -20,19 +20,19 @@ const getImages = images => {
 
 const Gallery = ({ location, data }) => {
   const images = data?.allFile.nodes.filter(node =>
-    node.relativePath.includes(location.state.folderName)
+    node.relativePath.includes(location.state?.folderName)
   )
   return (
     <Layout>
       <Container>
-        <h2 className="text-center py-5">{location.state.folderName}</h2>
+        <h2 className="text-center py-5">{location.state?.folderName}</h2>
         <Container>
-          <Row>{getImages(images.slice(0, 3))}</Row>
-          <Row>{getImages(images.slice(3, 6))}</Row>
-          <Row>{getImages(images.slice(6, 9))}</Row>
-          <Row>{getImages(images.slice(9, 12))}</Row>
-          <Row>{getImages(images.slice(12, 15))}</Row>
-          <Row>{getImages(images.slice(15, 18))}</Row>
+          <Row>{getImages(images?.slice(0, 3))}</Row>
+          <Row>{getImages(images?.slice(3, 6))}</Row>
+          <Row>{getImages(images?.slice(6, 9))}</Row>
+          <Row>{getImages(images?.slice(9, 12))}</Row>
+          <Row>{getImages(images?.slice(12, 15))}</Row>
+          <Row>{getImages(images?.slice(15, 18))}</Row>
         </Container>
       </Container>
     </Layout>
