7eb6f3ec54e7d1f22b02fd329ff29a4c48ee75e6
Sudharaka <sudharakap@mun.ca>
2022-12-30 19:07:06 -0800

    feat: add zcal embed

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,7 +20,7 @@ const About = () => (
           As a member of St.John’s small but vibrant creative community, I think
           I bring a different perspective to my work. Whether I’m photographing
           the exceptional landscapes of the island, capturing the glamorous
-          moments of your special occation, I always strive to put the story
+          moments of your special occasion, I always strive to put the story
           first.
         </p>
 
@@ -31,13 +31,16 @@ const About = () => (
 
         <Container className="py-4 px-0">
           <h4>Get in touch.</h4>
-          <Link href="mailto:masen0222@gmail.com">masen0222@gmail.com</Link>
+          <div className="zcal-inline-widget"><a href="https://zcal.co/i/8w976dUD">Discovery Call - Schedule a meeting</a></div>
         </Container>
       </Container>
     </Container>
   </Layout>
 )
 
-export const Head = () => <Seo title="About" />
+export const Head = () => <>
+  <Seo title="About" />
+  <script type="text/javascript" async src="https://static.zcal.co/embed/v1/embed.js"></script>
+</>
 
 export default About
