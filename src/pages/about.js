3c6500ad43b152fe5cb5b7522e07c12837ba6596
Sudharaka <sudharakap@mun.ca>
2023-01-03 20:09:38 -0800

    feat: add about paragraphs

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,20 +17,37 @@ const About = () => (
       </h2>
       <Container className="py-5">
         <p>
-          As a member of St.John’s small but vibrant creative community, I think
-          I bring a different perspective to my work. Whether I’m photographing
-          the exceptional landscapes of the island, capturing the glamorous
-          moments of your special occasion, I always strive to put the story
-          first.
+          Hi there! I am Manujaya, a professional photographer specializing in
+          graduation, family, and portrait photography. With 10+ years of
+          experience in the business, I am the go-to photographer for unique
+          events and auspicious occasions in St. John’s.
         </p>
-
         <p>
-          Please contact me directly for projects or to inquire about purchasing
-          my work.
+          Keeping a candid approach and adopting a photojournalistic style, I
+          strive to capture genuine emotions and moments as they unfold like a
+          story. It does not matter if I am shooting a wedding, portrait, or
+          landscape; I love documenting each subject’s unique beauty and
+          personality. Photography has always fascinated me because it allows me
+          to meet so many beautiful families, especially children, and
+          immortalize their smiles in frames forever. My portfolio comprises
+          beautiful, dreamy, and timeless photographs, each of which evokes an
+          emotion and tells a story.
+        </p>
+        <p>
+          Whatever works for you works for me! Whether you need indoor photos,
+          outdoor portraits, or headshots for your maternity, engagement,
+          wedding, or formal business event, I have got you covered. In addition
+          to my regular services, I offer mini-sessions for special occasions
+          like Christmas, Easter, and fall. I am dedicated to delivering
+          exceptional service at an affordable cost, with same-day sneak peeks
+          of my work and final outputs within 2-3 business days. When I am not
+          behind the camera, I can be found at my office in St. John’s, where I
+          spend time enhancing the photographs of the precious moments of your
+          life.
         </p>
 
         <Container className="py-4 px-0">
-          <h4>Get in touch.</h4>
+          <h4>Get in touch...</h4>
           <Script
             type="text/javascript"
             src="https://static.zcal.co/embed/v1/embed.js"
